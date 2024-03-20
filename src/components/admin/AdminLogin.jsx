773804e7821a16e19def99c71dd7063cc0f43a6f
saadaziz0014 <80111445+saadaziz0014@users.noreply.github.com>
2024-03-20 13:36:21 +0500

    update size of admin login logo

--- src/components/admin/AdminLogin.jsx
+++ src/components/admin/AdminLogin.jsx
@@ -55,7 +55,7 @@ const AdminLogin = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-36 w-36"
+            className="mx-auto h-44 w-44"
             src="/GEN-Serve.png"
             alt="Your Company"
           />
